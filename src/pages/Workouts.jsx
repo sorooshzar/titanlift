f4d28e6641395226e20077a1c652df34abe4fdff
Soroosh Zarei <soroosh.zar@gmail.com>
2026-03-10 05:24:40 +0000

    File changes

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -179,6 +179,7 @@ export default function Workouts() {
             onEdit={handleEditWorkout}
             onDelete={handleDeleteWorkout}
             onDuplicate={handleDuplicateWorkout}
+            onArchive={handleArchiveWorkout}
             onStart={handleStartWorkout}
           />
         ))}
